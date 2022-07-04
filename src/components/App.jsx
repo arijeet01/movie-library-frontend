8c9aa2fa45107148cfb2d8641ba85e4b49bfdfd4
arijeet01 <arijeetnayak2912@gmail.com>
2022-07-04 11:52:00 +0530

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,6 +24,7 @@ function App() {
           <Routes>
             <Route path="/"  element={<Signinsignup setUserLogin={setUserLogin} />} />
             <Route path="/home"  element={ user_var ? <Home user={JSON.parse(user_var)} apikey={apikey} Logout={Logout}/>: <Signinsignup setUserLogin={setUserLogin} />} />
+            <Route path="/list/public/SSR" element={<PublicList/>}/>
           </Routes>
          </BrowserRouter>
          
