1f3058cc0f4e121ea1e7e72ad0afee5d22fb6f8e
arijeet01 <arijeetnayak2912@gmail.com>
2022-07-04 12:25:17 +0530

    changes

--- src/components/PublicList.jsx
+++ src/components/PublicList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 function PublicList(props){
-    const {listname}=props.match.params;
+    const {listname}=useParams();
     return(<div>
         Hello you are in {listname}
     </div>)
